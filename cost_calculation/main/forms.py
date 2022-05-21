c4da68048ce29b69651f24ed64f3f6ed4504e827
tumka <tumka5688g@gmail.com>
2022-05-21 12:53:10 +0300

    Работает верно

--- cost_calculation/main/forms.py
+++ cost_calculation/main/forms.py
@@ -2,12 +2,15 @@ from django import forms
 
 
 class CalculationForm(forms.Form):
-    cost_work = forms.FloatField(help_text='100',
-                                 label='Стоимость услуги',
+    cost_work = forms.FloatField(label='Стоимость услуги',
+                                 widget=forms.NumberInput(
+                                     attrs={'placeholder': '100'}),
                                  required=True, min_value=0.01)
-    count_human = forms.IntegerField(help_text='2',
-                                     label='Количество людей',
+    count_human = forms.IntegerField(label='Количество людей',
+                                     widget=forms.NumberInput(
+                                         attrs={'placeholder': '2'}),
                                      required=True, min_value=1)
-    count_task = forms.IntegerField(help_text='8',
-                                    label='Количество услуг',
+    count_task = forms.IntegerField(label='Количество услуг',
+                                    widget=forms.NumberInput(
+                                        attrs={'placeholder': '8'}),
                                     required=True, min_value=1)
